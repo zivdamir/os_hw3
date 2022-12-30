aee904d9c2f942236b5408446914c1f51f821bc5
Levi Rauchwerger <levidr776@gmail.com>
2022-12-30 15:45:49 +0200

    Hi ziv, I am sad, I hope your computer is ok,
    your code frind,
    Large L Levi.

--- server.c
+++ server.c
@@ -1,7 +1,6 @@
 #include "segel.h"
 #include "request.h"
 
-
 // 
 // server.c: A very, very simple web server
 //
@@ -12,14 +11,20 @@
 // Most of the work is done within routines written in request.c
 // queue waiting requests , queue working reqes
 
+// A thread which handles requests.
 typedef struct thread_worker
 {
-	int connfd;
-	pthread_t worker;
+	int connfd; // The current client's fd
+    int request_type; // wether the corrent request is a dynamic/static/.. request
+	pthread_t thread; // The working thread struct.
 }*ThreadWorker;
 
-int num_threads;
-ThreadWorker workers;
+// threads_num - number of working treads.
+int threads_num;
+
+// An Array of ThreadWorkers
+ThreadWorker* workers;
+
 // HW3: Parse the new arguments too
 void getargs(int *port,int* num_threads,int* queue_size, int argc, char *argv[])
 {
@@ -38,11 +43,15 @@ void getargs(int *port,int* num_threads,int* queue_size, int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-	int listenfd, connfd, port, clientlen; // num_threads- number of working htreads.
+    /**received code**/
+	int listenfd, connfd, port, clientlen;
 	struct sockaddr_in clientaddr;
-	int queue_size;
-	getargs(&port,&num_threads,&queue_size, argc, argv);
-	workers = (ThreadWorker)malloc(sizeof(*workers)*num_threads);
+
+	/**added by us**/
+	struct timeval time;
+    int queue_size;
+    getargs(&port,&threads_num,&queue_size, argc, argv);
+	workers = (ThreadWorker)malloc(sizeof(*workers)*threads_num);
 	if(workers == NULL)
 	{
 		perror("Malloc failed");
@@ -54,12 +63,29 @@ int main(int argc, char *argv[])
 	// HW3: Create some threads...
 	//
 
-	listenfd = Open_listenfd(port);
+    for(int i = 0; i<threads_num; i++)
+    {
+        pthread_create(workers[i]->thread, NULL, threadRequestHandlerWrapper, NULL);
+    }
+
+    listenfd = Open_listenfd(port);
+
+
 	while (1) {
 		clientlen = sizeof(clientaddr);
 		connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+        gettimeofday(&time, NULL);
 
-		//
+        if(/*there is space in the ques*/)
+        {
+            //push_queue(waiting_q, connfd, arrival, NOT_DISPATCHED);
+            //pthread_cond_signal(&c);
+        }
+        else
+        {
+            //activate no space procedure
+        }
+        //
 		// HW3: In general, don't handle the request in the main thread.
 		// Save the relevant info in a buffer and have one of the worker threads
 		// do the work.
